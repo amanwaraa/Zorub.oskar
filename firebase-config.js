0c32bbff9b62d3f4d790761fdf2cba88d2269331
Maroshop <homworkhhh25@gmail.com>
2026-04-26 11:54:49 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,18 @@
 window.CASHIER_FIREBASE_CONFIG = {
   firebaseConfig: {
-    apiKey: "AIzaSyCnLAY7zQyBy7gUuL9wszt9aEhiJgvRmxI",
-    authDomain: "shop-d52dc.firebaseapp.com",
-    databaseURL: "https://shop-d52dc-default-rtdb.firebaseio.com",
-    projectId: "shop-d52dc",
-    storageBucket: "shop-d52dc.appspot.com",
-    messagingSenderId: "97580537866",
-    appId: "1:97580537866:web:abc46e5a2f527b6300a7f3",
-    measurementId: "G-956RQMBP42"
+    apiKey: "AIzaSyAFV1QhNHzfetfrfg_C-vFWKmGo8toQ0pI",
+    authDomain: "zorub-ee652.firebaseapp.com",
+    databaseURL: "https://zorub-ee652-default-rtdb.firebaseio.com",
+    projectId: "zorub-ee652",
+    storageBucket: "zorub-ee652.firebasestorage.app",
+    messagingSenderId: "125000693386",
+    appId: "1:125000693386:web:aec04c4734a9ce601a1f73",
+    measurementId: "G-92GM5526WR"
   },
 
-  firebaseRoot: "cashier_units_pro_885766842",
+  firebaseRoot: "cashier_units_pro_zorub_885766842",
 
   fallbackRoots: [
-    "cashier_units_pro_885766842"
+    "cashier_units_pro_zorub_885766842"
   ]
 };
